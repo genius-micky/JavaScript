ecdc45ccdb75478f87732a171864b714ba1a6285
mickey_30 <95030115+genius-micky@users.noreply.github.com>
2025-06-07 20:12:13 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,3 +28,55 @@ let someNumber = 3
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ******************* Operations ***********
+
+let value = 3
+let negValue = -value 
+
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " mickey"
+
+let str3 = str1 + str2
+console.log(str3); // hello mickey
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); // 32
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(true+); // error
+console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 // dont write like this
+
+let gameCounter = 100
+
+gameCounter++; // preFix and PostFix
+
+console.log(gameCounter); // 101
+
+
+
+
+
+
+
+
+
+
+
