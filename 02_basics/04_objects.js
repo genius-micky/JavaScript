970ded92a67e4a183dd5e49633e14345ec077f6d
mickey_30 <95030115+genius-micky@users.noreply.github.com>
2025-06-08 19:58:36 +0000

    cconstructor method and singleton

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,59 @@
+// with the helps of constructor, singleton
+
+// const tinderUser = new Object() // singleton Object
+
+const tinderUser = {} //  non singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "mickey"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userFullName: {
+            firstname: "harsh",
+            lastname: "tandan"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullName);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+//const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+     {
+        id: 1,
+        email: "h@gmail.com"
+    },
+     {
+        id: 1,
+        email: "h@gmail.com"
+    },
+
+]
+
+users[1].email
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser)); // creates array inside array.
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
