4c72d2e9915a116a89207b16a473a589b53820b3
mickey_30 <95030115+genius-micky@users.noreply.github.com>
2025-06-10 18:28:45 +0000

    intro to hoisting function declaration. variable holding function

--- 02_basics/06_scopes.js
+++ 02_basics/06_scopes.js
@@ -13,3 +13,45 @@ console.log(a); // 100
 console.log(b);
 console.log(c); // 30
 
+// Nested sccope
+
+function one(){
+    const username = "mickey"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    console.log(website);
+    
+    two()
+}
+// one()
+
+if (true) {
+    const usernam = "mickey"
+    if (username === "mickey") {
+        const website = " youtube"
+        // console.log(usernam + website);
+        
+    }
+    // console.log(website); // error
+    
+}
+
+// console.log(usernam); // error
+
+// **************************** interesting *******************
+
+function addOne(num) {
+    return num + 1
+}
+
+addOne(5) // only gets returned not print
+
+const addTwo = function(num) {
+    return num + 2
+}
+
+addTwo(5)
