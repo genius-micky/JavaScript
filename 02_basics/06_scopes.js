f149dfcc44373481998cf87efd2cb4d3351ff90e
mickey_30 <95030115+genius-micky@users.noreply.github.com>
2025-06-10 18:16:28 +0000

    local and global scope var interruption

---
+++ 02_basics/06_scopes.js
@@ -0,0 +1,15 @@
+
+//{} // scopes when it comes with function or if-else
+
+let a = 100
+
+if (true) {
+    let a = 10
+const b = 20
+var c = 30 // this will get print 
+}
+
+console.log(a); // 100
+console.log(b);
+console.log(c); // 30
+
