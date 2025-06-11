e5b24aad3409284861352cc983133028151d93ec
mickey_30 <95030115+genius-micky@users.noreply.github.com>
2025-06-11 18:49:59 +0000

    if-else statement

--- 03_Control_Flow/one.js
+++ 03_Control_Flow/one.js
@@ -1,10 +1,65 @@
 // if
 
 const isUserLoggedIn = true
+const temperature = 41
 
-if (2 === "2"){
-    console.log("executed");
+// if (temperature === 41 ){
+//     console.log("less than 50");
+    
+// }
+// else { 
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("executed");
+
+
+// <, >, <=, >=, ==, !=, ===
+
+// const score = 200
+
+// if(score > 100 ) {
+//     const power = "fly"
+//     console.log(`User Power: ${power}`);
+    
+// }
+// console.log();
+
+
+const balance = 1000
+//  if (balance > 500 ) console.log("test");
+ 
+
+// nesting 
+
+// if (balance < 500 ) {
+//     console.log("less than");
+    
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// }else if (balance < 900){
+//     console.log("less than 900");
+    
+// } else {
+//     console.log(" less than 1200 ");
+    
+// }
+
+const userLoggedIn = true 
+const debitCard = true 
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 3==2) {
+    console.log("Allow to buy course");
     
 }
 
-// <, >, <=, >=, ==, !=, ===
+
+if (loggedInFromEmail || loggedInFromGoogle) {
+    console.log(" user logged in ");
+    
+}
+
+
